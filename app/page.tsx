64f68ac0cf9fd419bc4b80e46e0d76428b01ff77
Siddheya Kulkarni <115717746+Asymtode712@users.noreply.github.com>
2025-08-02 16:38:09 +0000

    minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
         </div>
         {/* <Video/> */}
         <div className="">
-          <Image src="/anime.gif" alt="gif" width={1550} height={750} className="w-full"></Image>
+          <Image src="/gif4x.png" alt="gif" width={1550} height={750} className="w-full"></Image>
         </div>
       </div>
       <div className="">
